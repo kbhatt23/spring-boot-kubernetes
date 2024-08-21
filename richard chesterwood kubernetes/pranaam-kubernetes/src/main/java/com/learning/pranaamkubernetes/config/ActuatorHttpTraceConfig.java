39f62bf49a91b1960db9defad31724f8406eb18e
kbhatt23 <kanishkeminem@gmail.com>
2024-08-22 00:12:50 +0530

    helm course

---
+++ richard chesterwood kubernetes/pranaam-kubernetes/src/main/java/com/learning/pranaamkubernetes/config/ActuatorHttpTraceConfig.java
@@ -0,0 +1,20 @@
+package com.learning.pranaamkubernetes.config;
+
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ActuatorHttpTraceConfig {
+
+	// @Profile("actuator-endpoints") /* if you want: register bean only if profile is set */
+	public class HttpTraceActuatorConfiguration {
+
+	    @Bean
+	    public HttpTraceRepository httpTraceRepository() {
+	        return new InMemoryHttpTraceRepository();
+	    }
+
+	}
+}
--- richard chesterwood kubernetes/pranaam-kubernetes/src/main/java/com/learning/pranaamkubernetes/controllers/HealthCheckController.java
+++ richard chesterwood kubernetes/pranaam-kubernetes/src/main/java/com/learning/pranaamkubernetes/controllers/HealthCheckController.java
@@ -2,6 +2,7 @@ package com.learning.pranaamkubernetes.controllers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,10 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthCheckController {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(HealthCheckController.class);
+	
+	@Value("${random.number}")
+	private int randomNumber;
 
 	@GetMapping
 	public String healthCheck() {
-		LOGGER.info("healthCheck called");
+		LOGGER.info("healthCheck called with id: "+randomNumber);
 		return "status ok";
 	}
 }
